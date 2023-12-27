284dd24bc65ad45af1582a392221311d44bd88b6
nahueltaibo <nahueltaibo@gmail.com>
2023-12-26 22:51:11 -0500

    Beta version to be time tested

--- src/main.cpp
+++ src/main.cpp
@@ -8,25 +8,22 @@
 // To be included only in main(), .ino with setup() to avoid `Multiple Definitions` Linker Error
 #include "ESP32_ISR_Servo.h"
 
-#if !defined(LED_BUILTIN)
-#define LED_BUILTIN 4 // Pin D4 mapped to pin GPIO4/FLASH LED of ESP32, control on-board FLASH LED
-#endif
-
+#define PIN_LED 2
 //------------SERVO SETTINGS----------------
 #define PIN_SERVO 12
-#define MIN_MICROS 500
-#define MAX_MICROS 2500
+#define MIN_MICROS 1000
+#define MAX_MICROS 2000
 #define SERVO_RESTING_POSITION 90
-#define SERVO_FEEDING_POSITION 120 // 0=max anticlockwise, 0=stopped, 180=max clockwise
+#define SERVO_FEEDING_POSITION 150 // 0=max anticlockwise, 0=stopped, 180=max clockwise
 int servoIndex = -1;
 
 //------------FEEDING SETTINGS----------------
 #define FEEDING_INTERVAL_IN_HOURS 12 // Change this to the interval you want to feed your fish
-#define FEEDING_DURATION_IN_MS 500   // For how long the servo will be active
+#define FEEDING_DURATION_IN_MS 200   // For how long the servo will be active
 
-unsigned long previousMillis = 0; // will store last time code was executed
-// const long interval = FEEDING_INTERVAL_IN_HOURS * 60 * 60 * 1000; // interval in milliseconds
-const long interval = 10 * 1000; // interval in milliseconds
+unsigned long previousMillis = 0;                                 // will store last time code was executed
+const long interval = FEEDING_INTERVAL_IN_HOURS * 60 * 60 * 1000; // interval in milliseconds
+// const long interval = 10 * 1000; // interval in milliseconds
 
 void configureServo()
 {
@@ -63,7 +60,7 @@ void feedFish()
   Serial.print(millis());
   Serial.println(F(" Start feeding fish..."));
 
-  ESP32_ISR_Servos.enable(servoIndex);
+  digitalWrite(PIN_LED, HIGH);
 
   ESP32_ISR_Servos.setPosition(servoIndex, SERVO_FEEDING_POSITION);
 
@@ -71,14 +68,41 @@ void feedFish()
 
   ESP32_ISR_Servos.setPosition(servoIndex, SERVO_RESTING_POSITION);
 
-  ESP32_ISR_Servos.disable(servoIndex);
-
   Serial.print(millis());
   Serial.println(F(" Done feeding fish."));
+
+  digitalWrite(PIN_LED, LOW);
+}
+
+void processSerialInput()
+{
+  if (Serial.available() > 0)
+  {
+    char key = Serial.read();
+
+    switch (key)
+    {
+    case 'f':
+      feedFish();
+      break;
+
+    case 'r':
+      Serial.println(F("Rebooting..."));
+      delay(1000); // Delay to allow the message to be sent
+      ESP.restart();
+      break;
+
+    default:
+      // Ignore other keys
+      break;
+    }
+  }
 }
 
 void setup()
 {
+  pinMode(PIN_LED, OUTPUT);
+
   Serial.begin(115200);
   while (!Serial)
     ;
@@ -95,4 +119,6 @@ void loop()
   {
     feedFish();
   }
+
+  processSerialInput();
 }
